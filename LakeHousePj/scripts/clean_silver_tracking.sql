15d436cffe0a8d16fd31e684ab7e82b111c0669d
Long Vu <longvu.2043@gmail.com>
2025-12-07 11:41:42 +0700

    xoa file khong can thiet

--- LakeHousePj/scripts/clean_silver_tracking.sql
+++
@@ -1,9 +0,0 @@
--- Clean Silver layer tracking logs
-DELETE FROM file_ingestion_log 
-WHERE layer = 'silver';
-
--- Show remaining records
-SELECT layer, COUNT(*) as record_count 
-FROM file_ingestion_log 
-GROUP BY layer 
-ORDER BY layer;
--- LakeHousePj/scripts/list_tracking_tables.sql
+++
@@ -1,5 +0,0 @@
--- List all tracking and ingestion tables
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = 'public' 
-ORDER BY table_name;
